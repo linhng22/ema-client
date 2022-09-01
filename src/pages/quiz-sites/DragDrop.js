ad5476f93892155fe6a0a441a4496cab8143a1e4
Tien Vu <tienv2510@gmail.com>
2022-09-02 02:22:00 +0700

    fix the error of displaying questions in Drag-drop page

--- src/pages/quiz-sites/DragDrop.js
+++ src/pages/quiz-sites/DragDrop.js
@@ -93,7 +93,7 @@ export default function DragDrop() {
         if (item.matched) {
             count++;
         };
-        if (isLoaded && count === questionData.questions.length) {
+        if (questionData.questions && count === questionData.questions.length) {
             finished = true;
         } 
     })
